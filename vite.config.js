bf8f02a902cd7407a3b660a3359ebdd202fa7393
Nicolas Altamirano <nicolasaltamirano025@gmail.com>
2026-06-20 22:06:11 +0100

    Disable PWA service worker completely to test auth

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      disable: true,
       registerType: 'autoUpdate',
       strategies: 'injectManifest',
       srcDir: 'src',
